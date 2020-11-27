fb875d41d0b92e54396bc1072560309b1332d02e
Hamid Yaftian <hamid.yaftian@gmail.com>
2020-11-27 18:30:08 +0330

    Feature: Add vuetify to the project

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,26 @@
 <template>
-    <div id="app">
-        <router-view />
-    </div>
+    <v-app>
+        <v-app-bar app flat>
+            <v-toolbar-title>Scrum Planning Helper</v-toolbar-title>
+        </v-app-bar>
+
+        <v-main>
+            <router-view />
+        </v-main>
+
+        <v-footer>
+            <v-col class="text-center" cols="12">
+                {{ new Date().getFullYear() }} — 
+                Made with
+                <v-icon color="red">mdi-heart</v-icon> 
+                by Hamid Yaftian.
+            </v-col>
+        </v-footer>
+    </v-app>
 </template>
 
 <script>
     export default {
-        name: 'App',
+        name: "App",
     };
 </script>
-
-<style lang="scss"></style>
